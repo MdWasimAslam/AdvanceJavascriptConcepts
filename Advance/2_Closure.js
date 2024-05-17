1a17256d66e6a6abf9a40d04c9bbaa77889cc59c
MdWasiAslam <wasim.aslam@nlbtech.com>
2024-05-17 19:21:16 +0530

    Closure Concepts

--- Advance/2_Closure.js
+++ Advance/2_Closure.js
@@ -19,11 +19,10 @@ const grandChild = child();
 
 
 
-//----- Question 
+//----- Question 1
 
 
 var createCounter = function(n) {
-    let val = n
     return function() {
         return n++
     };
@@ -31,6 +30,43 @@ var createCounter = function(n) {
 
 
  const counter = createCounter(10)
-console.log(counter())
-console.log(counter())
+// console.log(counter())
+// console.log(counter())
 
+
+
+//---------- Question 2 ------- Write a closure function access parent variable and return the sum by the child compoennt
+
+
+function out(outVal){
+    let outValue = outVal
+    return function(inVal){
+        console.log(outValue+inVal)
+    }
+}
+
+
+const inn = out(23)
+// inn(2)
+
+
+
+//------- Question 3 ----- Write a closure function with method function to add and retrive datas
+
+
+function mainFunc(){
+    let res=0;
+
+   return {
+    add:(num)=>{res+=num},
+    display:()=>{console.log(res)}
+   }
+
+}
+
+
+const op = mainFunc();
+op.add(3);
+op.add(5);
+op.add(2);
+op.display()
